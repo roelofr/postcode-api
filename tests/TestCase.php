0f6a9389cd233415a78d55f69e3082ea8fe64290
Roelof Roos <github@roelof.io>
2020-01-18 18:53:09 +0100

    Added big list of naughty strings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,10 +7,12 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Application;
+use JsonException;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Roelofr\PostcodeApi\Contracts\PostcodeApiContract;
 use Roelofr\PostcodeApi\ServiceProvider;
 use Roelofr\PostcodeApi\Services\PostcodeApiService;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -63,5 +65,34 @@ abstract class TestCase extends BaseTestCase
             'prefix'   => '',
         ]);
         $app['config']->set('postcode-api', include dirname(__DIR__) . '/lib/config.php');
+    }
+
+    /**
+     * Returns a JSON file from the lib/ folder
+     *
+     * @param string $name
+     * @return array
+     * @throws RuntimeException
+     */
+    protected function getLibraryFile(string $name): array
+    {
+        // Library path
+        $filePath = dirname(__DIR__) . "/lib/tests/{$name}.json";
+
+        // Error if not found
+        if (!\file_exists($filePath)) {
+            throw new RuntimeException("Cannot find lib/{$name}.json");
+        }
+
+        // Get conents
+        $contents = \file_get_contents($filePath);
+
+        try {
+            // Return JSON contents
+            return json_decode($contents, true, 512, \JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            // Error if damaged
+            throw new RuntimeException("Failed to read JSON from lib/{$name}.json", 0, $e);
+        }
     }
 }
